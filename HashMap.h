ef4b36762f0bcb89c324c03e7cf84c7eaca10eb9
evancanter <evancanter@utexas.edu>
2019-05-01 10:53:06 -0500

    full impl on vs code pls work on kamek

---
+++ HashMap.h
@@ -0,0 +1,146 @@
+//string HashMap
+
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <algorithm>
+
+using namespace std;
+
+class HashMap {
+    const static int m = 128189;
+    public:
+
+        struct node {
+            node * next;
+            string fileName;
+        };
+        //typedef struct node node_t;
+
+        HashMap() {
+            for(int i =0;i<m;i++) {
+                map[i]=NULL;
+            }
+        }
+        void insert(string s,string file) {
+            int index=hash(s);
+            //cout<<s<<" hashed into "<<index<<endl;
+            if(map[index]==NULL) {
+                node *tmp=new node();
+                tmp->fileName=file;
+                map[index]=tmp;
+            }
+            else {
+                //cout<<"collision with " <<s<<" in "<<file<<" and "<<map[index]->fileName<<endl;
+                node *tmp=new node();
+                tmp->fileName=file;
+                tmp->next=map[index];
+                map[index]=tmp;
+            }
+        }
+
+        node* search(string s,string file) {
+            int index=hash(s);
+            node *p = map[index];
+            while(p!=NULL) {
+                if(p->fileName==file)
+                    return p;
+                p=p->next;
+            }
+            return NULL;
+        }
+        int getIndex(string fileName, vector<string> files) {
+            for(int i =0;i<files.size();i++) {
+                if(fileName==files[i]) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        void getCollisions(vector<string> files) {
+            int collisions[files.size()][files.size()];
+            for(int i =0;i<files.size();i++) {
+                for(int j=0;j<files.size();j++) {
+                    collisions[i][j]=0;
+                }
+            }
+            for(int i =0;i<m;i++) {
+                node* p=map[i];
+                while(p!=NULL) {
+                    int wrkIndex=getIndex(p->fileName,files);
+                    node * q=p->next;
+                    while(q!=NULL) {
+                        int second=getIndex(q->fileName,files);
+                        //cout<<wrkIndex<< " x "<<second<<endl;
+                        if(second!=wrkIndex) { //upper triangle, add to array
+                            //cout<<"adding " <<wrkIndex<< ", "<<second<<endl;
+                            collisions[wrkIndex][second]++;
+                        }
+                        q=q->next;
+                    }
+                    p=p->next;
+                }
+                
+            }
+            
+            vector<string> outputs;
+            for(int i =0;i<files.size();i++) {
+                for(int j=0;j<i-1;j++) {
+                    int col=collisions[i][j];
+                    if(col>=200&&col<1000) {
+                        string out=" "+std::to_string(col)+ " : "+files[j]+", "+files[i];
+                        outputs.push_back(out);
+                    }
+                    else if (col>=200) {
+                        string out=std::to_string(col)+ " : "+files[j]+", "+files[i];
+                        outputs.push_back(out);
+                    }
+                }
+            } 
+            sort(outputs.begin(),outputs.end());
+            for(int i =outputs.size()-1;i>=0;i--) {
+                cout<<outputs[i]<<endl;
+            }
+            /*
+            for(int i =0;i<files.size()-1;i++) {
+                for(int j=0;j<files.size();j++) {
+                    cout<<collisions[i][j]<<"   ";
+                }
+                cout<<endl;
+            }
+            */
+        }
+        ~HashMap() {
+            for(int i=0;i<m;i++) {
+                node *p = map[i];
+                node *prev=NULL;
+                while(p!=NULL) {
+                    delete prev;
+                    prev=p;
+                    p=p->next;
+                }
+            }
+        }
+    private:
+        node *map[m];
+        int hash(string chunk) {
+            int code=0;
+            for(int i=0;i<chunk.size();i++) {
+                int p=1;
+                for(int j=0;j<chunk.size()-i;j++) {
+                    p*=31;
+                }
+                code+=p*chunk[i];            
+            }
+            if(code<0) {code*=-1;}
+            return code%m;
+        }
+        
+        
+
+};
---
+++ cheaters.cpp
@@ -0,0 +1,130 @@
+//plagarismCatcher
+
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "HashMap.h"
+using namespace std;
+
+/*function... might want it in some class?*/
+int getdir (string dir, vector<string> &files)
+{
+    DIR *dp;
+    struct dirent *dirp;
+    if((dp  = opendir(dir.c_str())) == NULL) {
+        cout << "Error(" << errno << ") opening " << dir << endl;
+        return errno;
+    }
+
+    while ((dirp = readdir(dp)) != NULL) {
+        files.push_back(string(dirp->d_name));
+    }
+    closedir(dp);
+    for(vector<string>::iterator i=files.begin();i!=files.end();i++){
+        if(*i==".." || *i==".") {
+            files.erase(i);
+            i--;
+        }
+    }
+
+    return 0;
+}
+
+string minifyChunk(vector<string> rawChunk) {
+    string chunk="";
+    for(vector<string>::iterator i=rawChunk.begin();i!=rawChunk.end();++i) {
+        string word=*i;
+        for(string::iterator j=word.begin();j!=word.end();++j) {
+            int asciiVal=(int)*j;
+            if(asciiVal>64 && asciiVal<91) {//capital letter
+                asciiVal+=32;
+                *j=asciiVal;
+            }
+            else if(asciiVal<97 || asciiVal >122) {//punctuation
+                word.erase(j);
+                j--;
+            }
+        }
+        chunk+=word;
+    }
+    return chunk;
+}
+void chunk(string dir,vector<string> &files,int n,HashMap &h) {
+    //cout<<"start"<<endl;
+
+    //autocomplete dir
+    if(dir[dir.size()-1] != '/') {
+        dir+="/";
+    }
+
+    for(vector<string>::iterator i=files.begin();i!=files.end();++i) {
+
+        ifstream infile;
+        infile.open(dir+(*i),ios::in);
+        vector<string> words;
+        string temp="";
+        while(infile>>temp) {words.push_back(temp);}
+        //cout<<"words size: "<<words.size()<<endl;
+
+
+
+        for(vector<string>::iterator j=words.begin();j!=words.end()-n;++j) {
+
+            vector<string> rawChunk;
+            for(int k=0;k<n;k++) {
+                rawChunk.push_back(*(j+k));
+            }
+            h.insert(minifyChunk(rawChunk),*i);
+
+        }
+        //get first word
+        
+        words.clear();
+        infile.close();
+    }
+}
+
+void printCollisions(vector<string> files,HashMap h) {
+    int collisions[files.size()][files.size()];
+    for(int i=0;i<files.size();i++) {
+
+    }
+}
+
+int main(int argc,char* argv [])
+{
+    if(argc<3) {
+        cout<<"Error: please use syntax ./plagiarismCatcher [path/to/files] [chunk size]"<<endl;
+        return -1;
+    }
+    string dir = argv[1];
+    vector<string> files = vector<string>();
+    getdir(dir,files);
+   
+    // for (unsigned int i = 0;i < files.size();i++) {
+    //     cout << i << files[i] << endl;
+    // }
+   
+   /*
+   ifstream test;
+   test.open("sm_doc_set/"+files.front(),ios::in);
+   string s;
+    while(test>>s) {
+   cout<<s<<endl; }
+   
+   */
+   
+   HashMap h;
+   //h.insert("testonetwothreefourfiv3","file");
+   //cout<<h.search("testonetwothreefodurfiv3","file");
+   
+    int n=atoi(argv[2]);
+    chunk(dir,files,n,h);
+    h.getCollisions(files);
+    return 0;
+
+}
